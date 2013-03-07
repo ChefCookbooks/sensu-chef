6d2b3908636145065166e60a8751266ce89ef835
Tom Hite <thite@momentumsi.com>
2013-03-07 16:22:04 +0000

    Removes ssl configuration from config.json because empty settings break Sensu if not using ssl.

--- providers/base_config.rb
+++ providers/base_config.rb
@@ -3,6 +3,12 @@ action :create do
     !%w[rabbitmq redis api dashboard].include?(key.to_s) || value.nil?
   end
 
+  # Remove the rabbitmq ssl key if we don't want ssl (otherwise sensu breaks)
+  if !node.sensu.use_ssl
+    Chef::Log.warn("Sensu: use_ssl is set to false so removing all ssl settings from the sensu::rabbitmq configuration.")
+    definitions["rabbitmq"].delete("ssl")
+  end
+
   sensu_json_file ::File.join(node.sensu.directory, "config.json") do
     mode 0644
     content definitions
